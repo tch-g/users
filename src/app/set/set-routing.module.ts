51e3c4934186ab63872ec625ea6ff03c05ae1246
tch <tchighladze3@gmail.com>
2022-03-16 16:27:57 +0400

    routs

---
+++ src/app/set/set-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+  
+const routes: Routes = [
+//   {
+      
+//     path: '',
+//     redirectTo: 'list',
+//     pathMatch: "full"
+// },
+//   {
+//     path: 'create',
+//     component: CreateComponent
+//   },
+  
+ 
+];
+  
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SetRoutingModule { }
---
+++ src/app/set/set.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SetRoutingModule } from './set-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SetRoutingModule
+  ]
+})
+export class SetModule { }
