06e131087b3190d9d8d1a5ae4d3e0ce8c022a39e
tch <tchighladze3@gmail.com>
2022-03-16 16:31:46 +0400

    router components

--- src/app/set/set-routing.module.ts
+++ src/app/set/set-routing.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
 
 
   
 const routes: Routes = [
-//   {
+  {
       
-//     path: '',
-//     redirectTo: 'list',
-//     pathMatch: "full"
-// },
-//   {
-//     path: 'create',
-//     component: CreateComponent
-//   },
+    path: '',
+    redirectTo: 'list',
+    pathMatch: "full"
+  },
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
   
  
 ];
--- src/app/set/set.module.ts
+++ src/app/set/set.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SetRoutingModule } from './set-routing.module';
+import { UsersComponent } from './users/users.component';
+import { ListComponent } from './list/list.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersComponent,
+    ListComponent,
+    PostsComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
