3fabbae29ce931746834e7b51de70b1164b4c5b8
tch <tchighladze3@gmail.com>
2022-03-16 16:20:03 +0400

    create service

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+
+
+@Injectable({
+  providedIn: "root",
+})
+export class ApiService  {
+ 
+
+
+  constructor(private http: HttpClient,) {}
+  
+  getUsers(): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/users`);
+  }
+
+  getPosts(): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+  }
+
+  find(id:number): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+  }
+
+
+  
+}
